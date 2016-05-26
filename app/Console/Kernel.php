67594cd5f9f2a19ed336a98f1e0d042c61e0c4cd
Álvaro Baño Fos <alvarobanofos@gmail.com>
2016-05-26 20:11:36 +0200

    schedule task for finished questions

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,9 @@
 
 namespace App\Console;
 
+use App\QuestionsModel;
+use App\Revision;
+use Carbon\Carbon;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -24,7 +27,56 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('inspire')
-                 ->hourly();
+        $schedule->call(function () {
+            $this->controlFinishedQuestions();
+        })->everyMinute();
+    }
+
+    protected function controlFinishedQuestions($sleepQuestion = null, $oneMinuteLess=null, $oneMinuteMore=null) {
+
+        if($sleepQuestion) {
+            $sleepQuestion->finished = 1;
+            $sleepQuestion->save();
+            Revision::updateRevision();
+
+        }
+
+        $now = time();
+        if(!$oneMinuteLess)
+            $oneMinuteLess = Carbon::parse($now-60)->format('Y-m-d H:i:s');
+        if(!$oneMinuteMore)
+            $oneMinuteMore = Carbon::parse($now+60)->format('Y-m-d H:i:s');
+
+        $questions = QuestionsModel::where("time_ini", ">=", $oneMinuteLess)
+            ->where("time_ini", "<=", $oneMinuteMore)
+            ->where("finished", 0)
+            ->get();
+
+        $sleepTime = null;
+        $sleepQuestion = null;
+        foreach($questions as $question) {
+            $time_ini = Carbon::createFromFormat('Y-m-d H:i:s', $question->time_ini)->timestamp;
+            $time_end = $time_ini + $question->duration;
+
+
+            if($time_end > time()) {
+                $sleep = time() - $time_end;
+                if(!$sleepTime || $sleepTime < $sleep) {
+                    $sleepTime = $sleep;
+                    $sleepQuestion = $question;
+                }
+            }
+            else {
+                $question->finished = 1;
+                $question->save();
+                Revision::updateRevision();
+            }
+        }
+
+        if($sleepTime) {
+            sleep($sleepTime);
+            $this->controlFinishedQuestions($sleepQuestion, $oneMinuteLess, $oneMinuteMore);
+        }
+
     }
 }
