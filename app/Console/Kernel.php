de4078f57ad71542e86fe104517e9b0e6c01a086
Álvaro Baño Fos <alvarobanofos@gmail.com>
2016-05-27 01:23:27 +0200

    schedule task for finished questions

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -49,6 +49,7 @@ class Kernel extends ConsoleKernel
         if(!$oneMinuteMore)
             $oneMinuteMore = Carbon::createFromTimestamp($now+60, 'Europe/Madrid')->format('Y-m-d H:i:s');
 
+        dd($oneMinuteLess);
         $questions = QuestionsModel::where("time_ini", ">=", $oneMinuteLess)
             ->where("time_ini", "<=", $oneMinuteMore)
             ->where("finished", 0)
@@ -57,7 +58,6 @@ class Kernel extends ConsoleKernel
         $sleepTime = null;
         $sleepQuestion = null;
         foreach($questions as $question) {
-            dd($question);
             $time_ini = Carbon::createFromFormat('Y-m-d H:i:s', $question->time_ini, 'Europe/Madrid')->timestamp;
             $time_end = $time_ini + $question->duration;
 
